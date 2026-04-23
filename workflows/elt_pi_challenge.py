22a83af178c78c683c98cb444c87d11965e057da
José Molina <jose.molina@piconsulting.com.ar>
2026-04-23 20:06:15 -0300

    se corrige el dag de eventos

--- workflows/elt_pi_challenge.py
+++ workflows/elt_pi_challenge.py
@@ -2,32 +2,53 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.providers.http.operators.http import HttpOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
 
 PROJECT_ID = "project-1d5fe13d-8034-4422-8f5"
-LOCATION = "southamerica-east1"
+REGION = "us-east1"
+CLUSTER_NAME = "my-demo-cluster3"
 
-BRONZE_DATASET = "bronze_dataset"
-SILVER_DATASET = "silver_dataset"
-GOLD_DATASET = "gold_dataset"
+BUCKET = "pi-data-challenge-2026"
 
 default_args = {
     "owner": "jose",
     "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
 
+BRONZE_TO_SILVER_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {
+        "main_python_file_uri": f"gs://{BUCKET}/jobs/bronze_to_silver.py",
+    },
+}
+
+SILVER_TO_GOLD_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {
+        "main_python_file_uri": f"gs://{BUCKET}/jobs/silver_to_gold.py",
+    },
+}
+
+EXECUTION_LOGS_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {
+        "main_python_file_uri": f"gs://{BUCKET}/jobs/execution_logs.py",
+    },
+}
+
 with DAG(
-    dag_id="elt_pi_challenge",
+    dag_id="elt_pi_challenge_spark",
     default_args=default_args,
-    description="Orquestacion ELT: Cloud Run + BigQuery",
-    start_date=datetime(2026, 4, 16),
+    description="ELT pipeline with Cloud Run ingestion and Dataproc Spark transformations",
+    start_date=datetime(2026, 4, 23),
     schedule=None,
     catchup=False,
-    tags=["gcp", "elt", "challenge"],
+    tags=["gcp", "spark", "dataproc", "elt"],
 ) as dag:
 
     copy_data_to_bronze = HttpOperator(
@@ -54,70 +75,26 @@ with DAG(
         log_response=True,
     )
 
-    build_silver = BigQueryInsertJobOperator(
-        task_id="build_silver",
-        configuration={
-            "query": {
-                "query": f"""
-CREATE OR REPLACE TABLE `{PROJECT_ID}.{SILVER_DATASET}.unificado_clean` AS
-
-WITH combined AS (
-  SELECT *, 1 AS priority
-  FROM `{PROJECT_ID}.{BRONZE_DATASET}.unificado`
-
-  UNION ALL
-
-  SELECT
-    CHROM,
-    cast(POS as string),
-    ID,
-    REF,
-    ALT,
-    cast(QUAL as string),
-    `FILTER`,
-    INFO,
-    FORMAT,
-    cast(MUESTRA as string),
-    VALOR,
-    ORIGEN,
-    timestamp_seconds(cast(FECHA_COPIA as int64)),
-    RESULTADO,
-    2 AS priority
-  FROM `{PROJECT_ID}.{BRONZE_DATASET}.nuevas_filas`
-),
-
-dedup AS (
-  SELECT *,
-    ROW_NUMBER() OVER (
-      PARTITION BY id
-      ORDER BY priority DESC
-    ) AS rn
-  FROM combined
-)
-
-SELECT *
-FROM dedup
-WHERE rn = 1
-                """,
-                "useLegacySql": False,
-            }
-        },
-        location=LOCATION,
+    bronze_to_silver = DataprocSubmitJobOperator(
+        task_id="bronze_to_silver",
+        project_id=PROJECT_ID,
+        region=REGION,
+        job=BRONZE_TO_SILVER_JOB,
     )
 
-    build_gold = BigQueryInsertJobOperator(
-        task_id="build_gold",
-        configuration={
-            "query": {
-                "query": f"""
-CREATE OR REPLACE TABLE `{PROJECT_ID}.{GOLD_DATASET}.unificado_final` AS
-SELECT *
-FROM `{PROJECT_ID}.{SILVER_DATASET}.unificado_clean`
-                """,
-                "useLegacySql": False,
-            }
-        },
-        location=LOCATION,
+    silver_to_gold = DataprocSubmitJobOperator(
+        task_id="silver_to_gold",
+        project_id=PROJECT_ID,
+        region=REGION,
+        job=SILVER_TO_GOLD_JOB,
     )
 
-    copy_data_to_bronze >> csv_to_parquet >> sqlserver_to_parquet >> build_silver >> build_gold
+    execution_logs = DataprocSubmitJobOperator(
+        task_id="execution_logs",
+        project_id=PROJECT_ID,
+        region=REGION,
+        job=EXECUTION_LOGS_JOB,
+    )
+
+    [copy_data_to_bronze, sqlserver_to_parquet] >> csv_to_parquet
+    csv_to_parquet >> bronze_to_silver >> silver_to_gold >> execution_logs
